06383959713f03892f50c4ca8e1a230679ba70ef
JenJenUwU <75403961+JenJenUwU@users.noreply.github.com>
2022-10-23 00:16:38 -0400

    import command, import still to be fixed

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
-import discord
-from discord.ext import commands
+import os
+import nextcord
+import wavelink
+from nextcord.ext import commands
 from config import botPrefix
 from dotenv import load_dotenv
-import os
+
 # environment variables
 load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
-bot = commands.Bot(command_prefix=botPrefix, intents=discord.Intents.all())
+bot = commands.Bot(command_prefix=botPrefix, intents=nextcord.Intents.all())
 
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     print('------')
+    bot.loop.create_task(node_connect())
 
 
-# import bot token from github environment secret\
+@bot.event
+async def on_wavelink_node_ready(node: wavelink.Node):
+    print(f'Node {node.identifier} is ready.')
+
+
+async def node_connect():
+    await bot.wait_until_ready()
+    await wavelink.NodePool.create_node(bot=bot, host='lava.islantay.tk', port='true', password='AmeliaWatsonisTheBest**!', secure='True')
+
+
+@bot.command()
+async def play(ctx: commands.Context, *, search: wavelink.YouTubeTrack):
+    if not ctx.voice_client:
+        vc: wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
+    elif not ctx.author.voice_client:
+        return await ctx.send("You are not connected to a voice channel.")
+    else:
+        vc: wavelink.Player = ctx.voice_client
+    vc.play(search)
+# import bot token from github environment secret
 bot.run(token)
